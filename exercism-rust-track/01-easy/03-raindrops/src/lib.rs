15ab5b53647e42ccfea47b1c00efb7360cb41690
Attila Bagossy <bagossyattila@outlook.com>
2020-12-17 13:55:28 +0100

    [Exercism] Completed the Raindrops exercise

---
+++ exercism-rust-track/01-easy/03-raindrops/src/lib.rs
@@ -0,0 +1,21 @@
+pub fn raindrops(n: u32) -> String {
+    let mut result = String::new();
+    
+    if n % 3 == 0 {
+        result += "Pling"
+    }
+
+    if n % 5 == 0 {
+        result += "Plang"
+    }
+
+    if n % 7 == 0 {
+        result += "Plong"
+    }
+
+    if result == "" {
+        result = n.to_string()
+    }
+
+    result
+}
