0ee31f7f7eadd1e42d5e4a6945e60fead89a6077
Attila Bagossy <bagossyattila@outlook.com>
2020-12-17 12:01:06 +0100

    Declared some variables, both immutable and mutable

---
+++ rust-in-motion/unit-1/variables/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let x = 5;
+    let y = 6;
+    let z = x + y;
+    println!("z is {}", z);
+
+    let mut k = 5;
+    k += 1;
+    println!("k is {}", k);
+
+    let i: i32 = 5;
+    println!("i is {}", i);
+}
