4b7020bd6d59d9d15b2c9235d0910880b3e72584
Attila Bagossy <bagossyattila@outlook.com>
2020-12-22 10:09:14 +0100

    [The Book] Added the rectangles example of Chapter 5

---
+++ the-rust-programming-language/rectangles/src/main.rs
@@ -0,0 +1,52 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45
+    };
+
+    println!("rect1 is {:#?}", rect1);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let sq1 = Rectangle::square(10);
+    println!("sq1 is {:#?}", sq1);
+}
