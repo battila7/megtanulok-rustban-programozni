062ca9537ad51c4d064c99da3fcd736bec3dddda
Attila Bagossy <bagossyattila@outlook.com>
2020-12-23 11:38:29 +0100

    [Exercism] Refactored the High Scores solution a bit

--- exercism-rust-track/01-easy/14-high-scores/src/lib.rs
+++ exercism-rust-track/01-easy/14-high-scores/src/lib.rs
@@ -9,7 +9,10 @@ pub struct HighScores {
 impl HighScores {
     pub fn new(scores: &[u32]) -> Self {
         let mut sorted_scores = scores.to_vec();
-        sorted_scores.sort_by(|a, b| b.cmp(a));
+        // Reverse comparison so that
+        // sorted_scores will contain the scores
+        // in decreasing order.
+        sorted_scores.sort_unstable_by(|a, b| b.cmp(a));
 
         HighScores {
             scores: scores.to_vec(),
@@ -22,14 +25,21 @@ impl HighScores {
     }
 
     pub fn latest(&self) -> Option<u32> {
-        self.scores.last().map(|&x| x)
+        // first returns Option<&u32>
+        // which we can turn into n Option<u32>
+        // by using cloned as cloned
+        // clones the contents of the Option.
+        self.scores.last().cloned()
     }
 
     pub fn personal_best(&self) -> Option<u32> {
-        self.sorted_scores.first().map(|&x| x)
+        self.sorted_scores.first().cloned()
     }
 
     pub fn personal_top_three(&self) -> Vec<u32> {
+        // Vec.truncate works in place, truncating the
+        // original Vec, thus we are better off creating
+        // and copying a slice.
         let top_count = cmp::min(self.sorted_scores.len(), 3);
         
         self.sorted_scores[0..top_count].to_vec()
